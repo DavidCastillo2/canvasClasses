b90eb12266b4e99a55a53b4f0c1da6f61c111b89
DavidCastillo2 <58519904+DavidCastillo2@users.noreply.github.com>
2022-02-27 01:12:41 -0500

    Added canvas URL

--- DiscordBot/BotCommands.py
+++ DiscordBot/BotCommands.py
@@ -32,41 +32,8 @@ class classCommands(commands.Cog):
                         message = '```%s has a NEW assigment!\n\t' \
                                   'Course Title:\t%s\n\n\t' \
                                   'Name   :\t%s\n\t' \
-                                  'DueDate:\t%s\n```' % (
-                                      word, courseName, assignment.name, dueDate
+                                  'DueDate:\t%s\n\t' \
+                                  'URL    :\t%s\n```' % (
+                                      word, courseName, assignment.name, dueDate, assignment.html_url
                                   )
                         await channel.send(message)
-
-
-'''
-    @commands.command()
-    async def Arbi(self, ctx):
-        arbiData = self.driver.getArbi()
-        if arbiData is None:
-            await ctx.send("```Current Arbitration data is still being parsed, check back later u lil bitch muffin```")
-        else:
-            retVal = "```Current Arbitration Information" + '\n__________________________________________\n\n'
-            retVal = retVal + "Node: " + arbiData['node']
-            retVal = retVal + "\nEnemy Type: " + arbiData['enemy']
-            retVal = retVal + "\nType: " + arbiData['type'] + "\n"
-            alert = self.arbi.getAlert()
-            if alert is None:
-                retVal += "Alert Level: NONE"
-            else:
-                retVal += "Alert Level: " + alert
-            retVal += "```"
-            await ctx.send(retVal)
-
-    @commands.command()
-    async def arbi(self, ctx):
-        await self.Arbi(ctx)
-
-    @commands.command()
-    async def commands(self, ctx):
-        helptext = "```"
-        for command in self.commands:
-            helptext += f"{command}\n"
-        helptext += "\nType !help command for more info on a command"
-        helptext += "```"
-        await ctx.send(helptext)
-'''
--- main.py
+++ main.py
@@ -2,26 +2,27 @@ from Calendar.CalendarManager import CalendarManager
 from Canvas.CanvasWrapper import CanvasWrapper
 from DiscordBot.theBot import MyBot
 
-
-b = MyBot(command_prefix="!")
-b.begin()
+testing = True
 
 
-'''
-can = CanvasWrapper()
-cm  = CalendarManager()
+if not testing:
+    b = MyBot(command_prefix="!")
+    b.begin()
 
 
-cs = can.getUpcomingAssignments()
-for key, val in cs.items():
-    if len(val) > 0:
-        print(key, end='\n\t')
+else:
+    can = CanvasWrapper()
+    cm  = CalendarManager()
+
+
+    cs = can.getUpcomingAssignments()
+    for key, val in cs.items():
         for v in val:
-            print(v.name, end='\n\t')
-            print(v.due_at_date, end='\n\t')
-        print()
+            for myKey, myVal in v.__dict__.items():
+                print(myKey, end='\n\t')
+                print(myVal)
+            print()
 
-
-cm.updateCalendar(can.getUpcomingAssignments())
-'''
+# TODO
+# Ask teachers hey, any reason the actual due date and the canvas due date are different?
 
